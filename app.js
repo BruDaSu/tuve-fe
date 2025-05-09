d00d152ab67c60bed1305b71894552adc04affda
Yaleth <60183377@certus.edu.pe>
2025-05-09 09:45:34 -0500

    agregar el conts(process.env.MONGO_URI)

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerJsDoc));
 app.use(express.json());
 app.use(cors());
 
-const MONGO_URI=process.MONGO_URI;
+const MONGO_URI=process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI).then(()=>{
     console.log('Se conecto exitosamente');
